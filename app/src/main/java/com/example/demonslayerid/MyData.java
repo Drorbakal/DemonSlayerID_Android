8b63ea0e266d877b292dd08beca42c557cfad764
DrorBakal <drorbakal17@gmail.com>
2024-02-11 20:56:35 +0200

    first commit

---
+++ app/src/main/java/com/example/demonslayerid/MyData.java
@@ -0,0 +1,34 @@
+package com.example.demonslayerid;
+
+public class MyData {
+    static String[] name_arr = {"Nezuko Kamado", "Tanjirou kamado", "Akaza", "Genya Shinazugawa", "Giyu Tomioka",
+            "Inosuke Hashibira", "Kanao Tsuyuri", "Muzan Kibutsuji", "Kyojuro Rengoku", "Zenitsu Agatsuma"};
+    static String[] version_arr = {"She is a demon and the younger sister of Tanjiro Kamado and one of" +
+            " the two remaining members of the Kamado family. Formerly a human, she was attacked " +
+            "and transformed into a demon by Muzan Kibutsuji."
+            , "He is a Demon Slayer in the Demon Slayer Corps, who joined to find a remedy to turn" +
+            " his sister, Nezuko Kamado, back into a human and to hunt down and kill demons," +
+            " and later swore to defeat Muzan Kibutsuji, the King of Demons, in order to prevent" +
+            " others from suffering the same fate."
+            , "He is a demon affiliated with the Twelve Kizuki, holding the position of Upper " +
+            "Rank Three Over a century ago, Akaza was a human by the name of Hakuji" +
+            ", a prodigious martial artist who trained under Keizo while nursing his daughter, " +
+            "Koyuki, who would later become Akaza's fiancée."
+            , "He is a Demon Slayer in the Demon Slayer Corps and the younger brother of the Wind " +
+            "Hashira, Sanemi Shinazugawa."
+            , "He is a Demon Slayer of the Demon Slayer Corps and the current Water Hashira."
+            , "He is also a Demon Slayer in the Demon Slayer Corps."
+            , "She is a Demon Slayer of the Demon Slayer Corps, Shinobu Kocho's Tsuguko and " +
+            "the adopted sister of Shinobu and Kanae Kocho."
+            , "He is the Demon King, the first of his kind, and the progenitor of all other demons in " +
+            "existence. A millennium ago during the Heian Era, Muzan was turned into a demon during an" +
+            " experimental treatment trying to cure his terminal illness, and his goal since then was " +
+            "to become eternal."
+            , " He was a Demon Slayer of the Demon Slayer Corps and the late Flame Hashira ."
+            , "He is also a Demon Slayer in the Demon Slayer Corps."};
+    static Integer[] drawable_arr = {R.drawable.nezuko,R.drawable.tanjiro,R.drawable.akaza,
+            R.drawable.genya,R.drawable.giyu,R.drawable.inosuke,R.drawable.kanao,R.drawable.muzan,
+            R.drawable.rengoku,R.drawable.zenitsu};
+    static Integer[] id = {0,1,2,3,4,5,6,7,8,9};
+
+}
